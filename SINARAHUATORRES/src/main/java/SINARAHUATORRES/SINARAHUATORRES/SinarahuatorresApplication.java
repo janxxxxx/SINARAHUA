7d1cae5915453313719148ad36476669b36c6a3d
Jans Sinarahua <43283601+janxxxxx@users.noreply.github.com>
2025-01-17 23:17:26 -0500

    primer comit

---
+++ SINARAHUATORRES/src/main/java/SINARAHUATORRES/SINARAHUATORRES/SinarahuatorresApplication.java
@@ -0,0 +1,13 @@
+package SINARAHUATORRES.SINARAHUATORRES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinarahuatorresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinarahuatorresApplication.class, args);
+	}
+
+}
---
+++ SINARAHUATORRES/src/test/java/SINARAHUATORRES/SINARAHUATORRES/SinarahuatorresApplicationTests.java
@@ -0,0 +1,13 @@
+package SINARAHUATORRES.SINARAHUATORRES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinarahuatorresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
